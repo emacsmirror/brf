6e6616ae6df789f16922df07be051de9e25ebb10
Mike Woolley <mike@bulsara.com>
2021-04-28 08:00:00 +0100

    Minor correction to previous change.

--- brf.el
+++ brf.el
@@ -26,7 +26,7 @@
 
 ;; Brf-mode adds functionality from the legendary programmer's editor `Brief' to Emacs.
 ;;
-;; It is not an emulation of Brief (there are plenty of those already), but rather
+;; It is not an emulation of `Brief' (there are plenty of those already), but rather
 ;; provides an accurate implementation in Emacs of specific features that I miss
 ;; from `Brief'.  Principally these are:
 ;;
